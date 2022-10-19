c085d43d4e91014eadc52e8a16cdc1f6f5cbdf37
Ignacio Gonzalez Chemes <nachogonza123@gmail.com>
2022-10-19 03:21:50 -0300

    Fix domain to ping-service directly

--- src/services/ping-service.ts
+++ src/services/ping-service.ts
@@ -1,11 +1,10 @@
 import { injectable } from "inversify";
-import { Pong } from '../domain/pong';
 import PingPongResponse from "../models/ping-pong-response";
 
 @injectable()
 export class PingService {
 
     async getPing(): Promise<PingPongResponse> {
-        return (new Pong()).unmarshal()
+        return new PingPongResponse('pong');
     }
 }
